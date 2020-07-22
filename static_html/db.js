3ce6d63cd188a5c7b9cf584a6be7fd601a529cbe
JamesPerreault <japerre@gmail.com>
2020-07-22 08:38:42 -0400

    Added main html files.

---
+++ static_html/db.js
@@ -0,0 +1,27 @@
+
+
+var dburl = '';
+var dbrecipe = dburl + "?method=recipe&id=" ;
+var dbing = dburl + "?method=ingredients&id=" ;
+var dbsearch = dburl + "?method=search&q=" ;
+
+function flatten_results(data) {
+      var flat = data.map ( row => {
+           var retval = {} ;
+           row.forEach( col => {
+              let value=col.Value ;
+		   /*
+	      if ( value == "<i>NULL</i>" ) {
+		      value = "" ;
+	      }
+	      else  */
+              if (col.Type == 4 || col.Type == 5 ) {
+                 value=Number(value) ;
+              }
+              retval[col.Name] = value ;
+           } ) ;
+           return retval ;
+       } ) ;
+       return flat ;
+}
+
